198c8f97e18ef0769442642462523a2d6def3e40
Molonia2002 <114032831+Molonia2002@users.noreply.github.com>
2025-02-12 00:55:23 +0100

    Update db.py

--- api/db.py
+++ api/db.py
@@ -1,1 +1,5 @@
-
+books = [
+    {"id": 1, "title": "The Hobbit", "author": "J.R.R. Tolkien"},
+    {"id": 2, "title": "1984", "author": "George Orwell"},
+    {"id": 3, "title": "To Kill a Mockingbird", "author": "Harper Lee"},
+]
